102048aa4191ea7ca41dd95dbacae33c504966c1
walchko <walchko@users.noreply.github.com>
2017-12-27 15:03:45 -0700

    manual deploy option

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,29 @@
 # fix: git push origin master --force
 #      where master is the branch name
 
-# git info
-USER="walchko"
-EMAIL="${USER}@users.noreply.github.com"
-
-# repo
+# repo and branch to save website too
 REPO="github.com/MarsUniversity/ece382"
-
-# branch to save website too
 BRANCH="gh-pages"
+
+# setup travis or local env for pushing html to webpage
+if [[ ${TRAVIS} ]]; then
+	TMP="../tmpdir"
+
+	# git info
+	USER="walchko"
+	EMAIL="${USER}@users.noreply.github.com"
+	git config user.email ${EMAIL}
+	git config user.name ${USER}
+	REMOTE="https://${GITHUB_TOKEN}@${REPO}"
+# setup for local manual push
+else
+	TMP="/var/tmp/tmpdir"
+	REMOTE="git@github.com:MarsUniversity/ece382.git"
+	rm -fr ${TMP}
+fi
 
 # website data is stored here:
 FOLDER="html"
-TMP="../tmpdir"
 
 set -e
 
@@ -45,8 +55,6 @@ echo "deployed at `date`" > README.md
 
 # setup new git repo
 git init
-git config user.email ${EMAIL}
-git config user.name ${USER}
 git add *
 git commit -m "Deploying to ${REPO}:${BRANCH}"
 
@@ -56,4 +64,7 @@ echo "======================"
 
 # force the contents of this master branch to gh-pages branch
 # git push --force --quiet "https://${GITHUB_TOKEN}@${REPO}" master:${BRANCH} > /dev/null 2>&1
-git push --force  "https://${GITHUB_TOKEN}@${REPO}" master:${BRANCH}
+# git push --force  "https://${GITHUB_TOKEN}@${REPO}" master:${BRANCH}
+# git push --force origin master:${BRANCH}
+
+git push --force ${REMOTE} master:${BRANCH}
